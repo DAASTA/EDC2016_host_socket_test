2551c32c949a66a54d19c2ae3ffa475a3f30af4b
sugar10w <sugar10w@163.com>
2016-10-29 19:27:07 +0800

    [update] 2016.10.29

--- src/main.cpp
+++ src/main.cpp
@@ -26,30 +26,28 @@ int main(int argc, char* argv[])
     SocketServer socket_server(30000, function);
     socket_server.run();
 
-    //char buffer[20] = { 0x0d, 0x0a, 0x00 };
-    //vector<string> head_list;
-    //head_list.push_back(buffer);
-    //SerialPortProtol protol(head_list, 20);
+    char tail[3] = { 0x0d, 0x0a, 0x00 };
+    SerialPortProtol protol(5, tail);
 
-    SerialPort port(3, 115200/*, protol*/);
+    SerialPort port(15, 115200, protol);
     
     while (true)
     {
         std::vector<MyString> list = port.receive();
         for (int i = 0; i < list.size(); ++i) 
-            printf("%s %s\n", TimeStamp::getTimeStamp().c_str(), list[i].c_str());
+            printf("%s %s\n", TimeStamp::getTimeStamp().c_str(), list[i].hex_str());
         if (list.size() > 0) shared_data = list[0];
 
         if (_kbhit()) {
             char ch = _getch();
             if (ch == ' ') {
-                printf("%s === 暂停: 等待输入 ===\n", TimeStamp::getTimeStamp().c_str());
+                printf("\n\n%s === 暂停: 等待输入 ===\n", TimeStamp::getTimeStamp().c_str());
                 //system("pause");
                 printf("输入要发送的字符(ASCII): \n");
                 char buffer[256];
                 std::cin.getline(buffer, 256);
                 port.send(MyString(buffer));
-                printf("%s === 运行中 - 正在持续接受数据，按空格键暂停 ===\n", TimeStamp::getTimeStamp().c_str());
+                printf("%s === 运行中 - 正在持续接受数据，按空格键暂停 ===\n\n", TimeStamp::getTimeStamp().c_str());
             }
         }
 
